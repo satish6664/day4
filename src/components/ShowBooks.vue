8ebf80623a54a7e9de8726739566ff76fc034c0c
satish6664 <satish1242@sasi.ac.in>
2024-02-14 11:26:42 +0530

    first commit

---
+++ src/components/ShowBooks.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="ShowBooks">
+        <h2>{{ title }}</h2>
+        <p> {{ price }}</p>
+    </div>
+    <div v-else>No Books</div>
+
+    <button @click="ToggleShowBooks">
+        <span v-if="ShowBooks">Hide Books</span>
+        <span v-else>Show Books</span>
+    </button>
+</template>
+<script setup>
+
+import { ref } from 'vue';
+    const ShowBooks=ref(true)
+    const title=ref("The Book")
+    const price=ref(40)
+
+    function ToggleShowBooks()
+    {
+        ShowBooks.value=!ShowBooks.value
+    }
+</script>
